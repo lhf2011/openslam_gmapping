b0c7b1dc508433256f7b091e765ed0fc82e16008
lhf2011 <lhf530001541@gmail.com>
2020-09-05 18:22:08 -0400

    add TSDF mapping
    TSDF is the abbreviation of Truncated Signed Distance Function, it is a volumetric representation of a scene for integrating depth images that has
    several benefits, e.g. time and space efficiency, representation of uncertainty or incremental updating.

--- gridfastslam/motionmodel.cpp
+++ gridfastslam/motionmodel.cpp
@@ -10,7 +10,8 @@ namespace GMapping {
 
 
 OrientedPoint 
-MotionModel::drawFromMotion (const OrientedPoint& p, double linearMove, double angularMove) const{
+MotionModel::drawFromMotion (const OrientedPoint& p, double linearMove, double angularMove) const
+{
 	OrientedPoint n(p);
 	double lm=linearMove  + fabs( linearMove ) * sampleGaussian( srr ) + fabs( angularMove ) * sampleGaussian( str );
 	double am=angularMove + fabs( linearMove ) * sampleGaussian( srt ) + fabs( angularMove ) * sampleGaussian( stt );
@@ -21,44 +22,36 @@ MotionModel::drawFromMotion (const OrientedPoint& p, double linearMove, double a
 	return n;
 }
 
-OrientedPoint 
-MotionModel::drawFromMotion(const OrientedPoint& p, const OrientedPoint& pnew, const OrientedPoint& pold) const{
-	double sxy=0.3*srr;
-	OrientedPoint delta=absoluteDifference(pnew, pold);
-	OrientedPoint noisypoint(delta);
-	noisypoint.x+=sampleGaussian(srr*fabs(delta.x)+str*fabs(delta.theta)+sxy*fabs(delta.y));
-	noisypoint.y+=sampleGaussian(srr*fabs(delta.y)+str*fabs(delta.theta)+sxy*fabs(delta.x));
-	noisypoint.theta+=sampleGaussian(stt*fabs(delta.theta)+srt*sqrt(delta.x*delta.x+delta.y*delta.y));
-	noisypoint.theta=fmod(noisypoint.theta, 2*M_PI);
-	if (noisypoint.theta>M_PI)
-		noisypoint.theta-=2*M_PI;
-	return absoluteSum(p,noisypoint);
+OrientedPoint MotionModel::drawFromMotion(const OrientedPoint& p, const OrientedPoint& pnew, const OrientedPoint& pold) const
+{
+    double sxy=0.3*srr;
+
+    // the motion at the interval of two frames, during t-1 and t
+    // both the pnew nad pold are from odometry
+    OrientedPoint delta=absoluteDifference(pnew, pold);
+
+    double alpha1=atan2(delta.y, delta.x)-pold.theta;
+    double alpha2=delta.theta-alpha1;
+
+    // noise estimation
+    OrientedPoint noisypoint(delta);
+
+    // mean 0 Gaussian, set covariance here
+    noisypoint.theta+=sampleGaussian(stt*fabs(alpha1)+srt*sqrt(delta.x*delta.x+delta.y*delta.y));
+    noisypoint.x+=sampleGaussian(srr*fabs(delta.x)+str*fabs(delta.theta)+sxy*fabs(delta.y));
+    noisypoint.y+=sampleGaussian(srr*fabs(delta.y)+str*fabs(delta.theta)+sxy*fabs(delta.x));
+    noisypoint.theta+=sampleGaussian(stt*fabs(alpha2)+srt*sqrt(delta.x*delta.x+delta.y*delta.y));
+
+    noisypoint.theta=fmod(noisypoint.theta, 2*M_PI);
+    if (noisypoint.theta>M_PI)
+        noisypoint.theta-=2*M_PI;
+
+    // add noise and return
+    return absoluteSum(p,noisypoint);
 }
 
-
-/*
-OrientedPoint 
-MotionModel::drawFromMotion(const OrientedPoint& p, const OrientedPoint& pnew, const OrientedPoint& pold) const{
-	
-	//compute the three stps needed for perfectly matching the two poses if the noise is absent
-	
-	OrientedPoint delta=pnew-pold;
-	double aoffset=atan2(delta.y, delta.x);
-	double alpha1=aoffset-pold.theta;
-	alpha1=atan2(sin(alpha1), cos(alpha1));
-	double rho=sqrt(delta*delta);
-	double alpha2=pnew.theta-aoffset;
-	alpha2=atan2(sin(alpha2), cos(alpha2));
-	
-	OrientedPoint pret=drawFromMotion(p, 0, alpha1);
-	pret=drawFromMotion(pret, rho, 0);
-	pret=drawFromMotion(pret, 0, alpha2);
-	return pret;
-}
-*/
-
-
-Covariance3 MotionModel::gaussianApproximation(const OrientedPoint& pnew, const OrientedPoint& pold) const{
+Covariance3 MotionModel::gaussianApproximation(const OrientedPoint& pnew, const OrientedPoint& pold) const
+{
 	OrientedPoint delta=absoluteDifference(pnew,pold);
 	double linearMove=sqrt(delta.x*delta.x+delta.y*delta.y);
 	double angularMove=fabs(delta.x);
